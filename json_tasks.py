0e92a082dbc55b30bba529ea0c9531caa7f5242d
agboytsov <133656833+agboytsov@users.noreply.github.com>
2023-05-30 15:06:32 +0300

    260523

--- json_tasks.py
+++ json_tasks.py
@@ -463,3 +463,78 @@ def is_correct_json(string):
 # Примечание 4. Указанный файл доступен по ссылке. Ответ на задачу доступен по ссылке.
 #
 # Примечание 5. При открытии файла используйте явное указание кодировки UTF-8.
+# import csv,json
+# with open('playgrounds.csv', 'r', encoding='UTF-8') as inp:
+#
+#     reader = list(csv.DictReader(inp, delimiter =';'))
+#
+# admareas = [str(i['AdmArea']) for i in reader]
+#
+# res = {i: {reader[j]['District']:[reader[k]['Address'] for k in range(len(reader)) if reader[k]['District'] == reader[j]['District'] ] for j in range(len(reader)) if reader[j]['AdmArea'] == i } for i in [str(i['AdmArea']) for i in reader]}
+# with open('addresses.json','w', encoding='UTF-8') as out:
+#     json.dump(res, out, indent=3, ensure_ascii=False)
+
+
+
+
+#4.4.12
+# Студенты курса
+# Вам доступен файл students.json, содержащий список JSON-объектов, которые представляют данные о студентах некоторого курса:
+#
+# [
+#    {
+#       "name": "Holly-Anne",
+#       "city": "Abilene",
+#       "age": 29,
+#       "progress": 85,
+#       "phone": "(802) 983-9126"
+#    },
+#    ...
+# ]
+# Под «студентом» мы будем подразумевать один JSON-объект из этого списка. У студента имеются следующие атрибуты:
+#
+# name — имя
+# city — город проживания
+# age — возраст
+# progress — прогресс по курсу в процентах
+# phone— контактный номер
+# Напишите программу, которая определяет студентов, удовлетворяющих следующим условиям:
+#
+# возраст
+# 18
+# 18 лет или более
+# прогресс по курсу
+# 75
+# %
+# 75% или более
+# Программа должна создать файл data.csv с двумя столбцами — name (имя) и phone (номер), и записать в него данные выбранных студентов, расположив их в лексикографическом порядке имён. В качестве разделителя в файле data.csv должна быть использована запятая.
+#
+# Примечание 1. Гарантируется, что все студенты имеют различные имена.
+#
+# Примечание 2. Начальная часть файла data.csv выглядит так:
+#
+# name,phone
+# Cary,(580) 476-8517
+# Catarina,(237) 608-2757
+# Catherin,(876) 215-3666
+# ...
+# Примечание 3. Указанный файл доступен по ссылке. Ответ на задачу доступен по ссылке.
+#
+# Примечание 4. При открытии файла используйте явное указание кодировки UTF-8.
+import json, csv
+with open('students.json','r',encoding='UTF-8') as f:
+    data = json.load(f)
+res = []
+for d in data:
+    if d['age'] >= 18 and d['progress'] >= 75:
+        res.append((d['name'], d['phone']))
+
+res = sorted(res, key=lambda x: x[0])
+
+
+out = 'datajsonout.csv'
+with open(out, 'w',encoding='UTF-8', newline='') as outf:
+    writer = csv.DictWriter(outf, fieldnames=['name','phone'], delimiter=',')
+    writer.writeheader()
+    for row in res:
+        writer.writerow({'name': row[0], 'phone': row[1]})
