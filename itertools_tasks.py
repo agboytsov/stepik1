3d9f7a801268e03055ed3179c543978db73e02be
Alexander Boytsov <133656833+agboytsov@users.noreply.github.com>
2023-09-04 14:17:04 +0300

    Update itertools_tasks.py

--- itertools_tasks.py
+++ itertools_tasks.py
@@ -849,6 +849,77 @@ combs = []
 # # Примечание 1. Рюкзак не обязательно должен быть наполнен полностью.
 # Примечание 2. Подробнее с задачей о рюкзаке можно ознакомиться по ссылке. https://ru.wikipedia.org/wiki/Задача_о_рюкзаке
 
+from collections import namedtuple
+import itertools as it
+
+Item = namedtuple('Item', ['name', 'mass', 'price'])
+
+items = [Item('Обручальное кольцо', 7, 49_000),
+         Item('Мобильный телефон', 200, 110_000),
+         Item('Ноутбук', 2000, 150_000),
+         Item('Ручка Паркер', 20, 37_000),
+         Item('Статуэтка Оскар', 4000, 28_000),
+         Item('Наушники', 150, 11_000),
+         Item('Гитара', 1500, 32_000),
+         Item('Золотая монета', 8, 140_000),
+         Item('Фотоаппарат', 720, 79_000),
+         Item('Лимитированные кроссовки', 300, 80_000)]
+
+def rukzak(items, mass):
+    max_value = 0
+    selected = []
+    for i in range(1,len(items)+1):
+        for comb in it.combinations(items, i):
+            total_weight = sum(item.mass for item in comb)
+            total_value = sum(item.price for item in comb)
+            if total_weight <= mass and total_value > max_value:
+                max_value = total_value
+                selected = list(comb)
+    return selected
+
+# mass = int(input())
+# selected_items = rukzak(items, mass)
+# selected_items = list(sorted(selected_items, key = lambda x: x.name))
+# if selected_items:
+#     for item in selected_items:
+#         print(item.name)
+# else:
+#     print('Рюкзак собрать не удастся')
+
+### examples
+# value = int(input())
+
+# if min(items, key=lambda x: x.mass).mass > value:
+#     print('Рюкзак собрать не удастся')
+# else:
+#     data = chain.from_iterable(combinations(items, r=i) for i in range(1, 11))
+#     data_filtered = filter(lambda x: value >= sum(i.mass for i in x), data)
+#     result = max(data_filtered, key=lambda x: sum(i.price for i in x))
+#     print(*sorted(i.name for i in result), sep='\n')
+
+
+###
+bag_weight = int(input())
+bag_price = -1
+bag_items = None
+
+for i in range(1, 11):
+    for comb in combinations(items, i):
+        comb_price = sum(item.price for item in comb)
+        comb_weight = sum(item.mass for item in comb)
+        if comb_price > bag_price and comb_weight <= bag_weight:
+            bag_items = comb
+            bag_price = comb_price
+
+if bag_items:
+    for item in sorted(bag_items):
+        print(item.name)
+else:
+    print('Рюкзак собрать не удастся')
+
+
+
+
 
 
 
